b65605ec1c7b236d4680176cca328f0d148cf6b5
youzenghe <2442616509@qq.com>
2025-05-14 15:48:00 +0800

    回顾练习2

---
+++ day514(算法题).py
@@ -0,0 +1,9 @@
+from fractions import Fraction
+
+# def sums(n):
+#     su = Fraction(0)
+#     for i in range(0,n):
+#         su += Fraction(1,2**i)
+#     print(su)
+if __name__ == '__main__':
+    # sums(20)
