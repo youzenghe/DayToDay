14bbc783280365d4e249c6ae9859c1c17618216b
youzenghe <2442616509@qq.com>
2025-05-14 20:42:40 +0800

    增加练习

--- day514(算法题).py
+++ day514(算法题).py
@@ -5,5 +5,17 @@ from fractions import Fraction
 #     for i in range(0,n):
 #         su += Fraction(1,2**i)
 #     print(su)
+
+# def method():
+#     cnt = 0
+#     i = 0
+#     while True:
+#         cnt+=str(i).count("1")
+#         if cnt>=2021:
+#             print(i)
+#             break
+#         i+=1
+
 if __name__ == '__main__':
     # sums(20)
+    # method()
